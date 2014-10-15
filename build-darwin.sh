1bb67e6dddb511b1af7600fb7495c98e635a9de3
kmariano <koshin.mariano@gmail.com>
2014-10-15 15:14:51 -0700

    Updating OSX script.

--- build-darwin.sh
+++ build-darwin.sh
@@ -6,7 +6,7 @@ mkdir dist
 mkdir tmp
 
 curl http://nodejs.org/dist/v0.10.32/node-v0.10.32-darwin-x64.tar.gz -o tmp/node-v0.10.32-darwin-x64.tar.gz
-tar -zxf tmp/node-v0.10.32-darwin-x64.tar.gz dist/node-v0.10.32-darwin-x64
+tar -zxf tmp/node-v0.10.32-darwin-x64.tar.gz --directory dist/
 
 curl http://dl.node-webkit.org/v0.10.5/node-webkit-v0.10.5-osx-x64.zip -o tmp/node-webkit-v0.10.5-osx-x64.zip
 unzip tmp/node-webkit-v0.10.5-osx-x64.zip -d tmp/
