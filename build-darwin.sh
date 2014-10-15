c66b19b42e672968685810bafa6782908e5d5092
kmariano <koshin.mariano@gmail.com>
2014-10-15 15:08:23 -0700

    Updating OSX script.

--- build-darwin.sh
+++ build-darwin.sh
@@ -3,11 +3,12 @@ git clone https://github.com/octoblu/gateblu-ui.git
 cd ./gateblu-ui
 npm install
 mkdir dist
-curl http://nodejs.org/dist/v0.10.32/node-v0.10.32-darwin-x64.tar.gz -o dist/node-v0.10.32-darwin-x64.tar.gz
-tar -zxf dist/node-v0.10.32-darwin-x64.tar.gz
-rm dist/node-v0.10.32-darwin-x64.tar.gz
+mkdir tmp
 
-curl http://dl.node-webkit.org/v0.10.5/node-webkit-v0.10.5-osx-x64.zip -o node-webkit-v0.10.5-osx-x64.zip
-unzip -d node-webkit-v0.10.5-osx-x64.zip
-rm node-webkit-v0.10.5-osx-x64.zip
-mv node-webkit gateblu
+curl http://nodejs.org/dist/v0.10.32/node-v0.10.32-darwin-x64.tar.gz -o tmp/node-v0.10.32-darwin-x64.tar.gz
+tar -zxf tmp/node-v0.10.32-darwin-x64.tar.gz dist/node-v0.10.32-darwin-x64
+
+curl http://dl.node-webkit.org/v0.10.5/node-webkit-v0.10.5-osx-x64.zip -o tmp/node-webkit-v0.10.5-osx-x64.zip
+unzip tmp/node-webkit-v0.10.5-osx-x64.zip -d tmp/
+
+mv tmp/node-webkit-v0.10.5-osx-x64/* .
