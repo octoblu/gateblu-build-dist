02e5e8c5319aeccff81ad57aab1a2e3656aa6196
redaphid <iam@aaronherres.com>
2014-10-15 17:42:15 -0700

    seems to work!

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-curl -O https://github.com/octoblu/gateblu-build-dist/archive/master.zip
+
+curl -L -O https://github.com/octoblu/gateblu-ui/archive/master.zip
 unzip master.zip
-mv master gateblu-ui
+mv gateblu-ui-master gateblu-ui
+rm master.zip
 
 cd ./gateblu-ui
 npm install
@@ -11,9 +13,10 @@ mkdir tmp
 curl -o tmp/node-v0.10.32-linux-x64.tar.gz http://nodejs.org/dist/v0.10.32/node-v0.10.32-linux-x64.tar.gz
 tar -zxf tmp/node-v0.10.32-linux-x64.tar.gz --directory dist/
 
-
 curl -o tmp/node-webkit-v0.10.5-linux-x64.tar.gz http://dl.node-webkit.org/v0.10.5/node-webkit-v0.10.5-linux-x64.tar.gz
-tar -zxf tmp/node-webkit-v0.10.5-linux-x64 --directory dist/
+tar -zxf tmp/node-webkit-v0.10.5-linux-x64.tar.gz --directory tmp/
 mv tmp/node-webkit-v0.10.5-linux-x64/* .
+
+sed -i 's/udev\.so\.0/udev.so.1/g' nw
 
 rm -rf ./tmp
