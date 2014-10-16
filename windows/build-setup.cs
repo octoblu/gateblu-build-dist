6d4ff02ef0cb5a503e7ae15d42553d6855b55e68
kmariano <koshin.mariano@gmail.com>
2014-10-16 13:11:22 -0700

    Adding ability to download files for Setup

--- windows/build-setup.cs
+++ windows/build-setup.cs
@@ -11,31 +11,69 @@ namespace Octoblu.Gateblu{
 		   
 		   string path = Directory.GetCurrentDirectory();
 		   string tmpPath = path + "\\tmp";
-		   string zipFilePath = tmpPath + "\\gateblu-ui.zip"; 
+		   string distPath = path + "\\gateblu-ui\\dist";
+		   string nodePath = distPath + "\\node-v0.10.32-win-x86";
 		   
-		   Directory.CreateDirectory(tmpPath); 
-		   Uri uri = new Uri("https://github.com/octoblu/gateblu-ui/archive/master.zip");
-		   
-		   WebRequest http = HttpWebRequest.Create(uri.AbsoluteUri);
-		   HttpWebResponse response = (HttpWebResponse)http.GetResponse();
-		   Stream stream = response.GetResponseStream();
-		   FileStream fileStream = new FileStream(zipFilePath, FileMode.Create);
-		   stream.CopyTo(fileStream); 
-		   response.Close(); 
-		   fileStream.Close(); 
-		   Console.WriteLine("Downloading Gateblu-UI ....");
-		   Console.WriteLine("Download Complete ....");
-		   Console.WriteLine("Extracting Gateblu-UI source ....");
-	   
-	       DecompressFile(zipFilePath, tmpPath);
+		   Directory.CreateDirectory(tmpPath);
+		   /*
+
+		   #http://dl.node-webkit.org/v0.10.5/node-webkit-v0.10.5-win-ia32.zip
+       #https://codeload.github.com/octoblu/gateblu-ui/zip/master
+       #http://nodejs.org/dist/v0.10.32/x64/node.exe
+       #http://nodejs.org/dist/npm/npm-1.4.12.zip
+       #$gatebluZipFileName
+       */
+
+	     DownloadAndDecompressFile("https://github.com/octoblu/gateblu-ui/archive/master.zip", tmpPath +"\\gateblu-ui.zip", tmpPath);
+
+	     Directory.Move(tmpPath + "\\gateblu-ui-master", path + "\\gateblu-ui");
+	     var directoryInfo = Directory.CreateDirectory(nodePath);
+
+       DownloadAndDecompressFile("http://dl.node-webkit.org/v0.10.5/node-webkit-v0.10.5-win-ia32.zip", tmpPath + "\\node-webkit-v0.10.5-win-ia32.zip", path)
+       DownloadAndDecompressFile("http://nodejs.org/dist/npm/npm-1.4.12.zip", tmpPath + "\\npm-1.4.12.zip",  nodePath);
+       DownloadFile("http://nodejs.org/dist/v0.10.32/x64/node.exe", nodePath );
+
+	      http = HttpWebRequest.Create(uri.AbsoluteUri);
+	      stream = response.GetResponseStream();
+        fileStream = new FileStream(zipFilePath, FileMode.Create);
+        stream.CopyTo(fileStream);
+        response.Close();
+        fileStream.Close();
+
+
+
+
+
+
+
+
+
 
 		}
 		
 		static void DecompressFile(string filename, string targetDirectory ){
 	        using (ZipFile zipFile = ZipFile.Read(filename))
 	        {
-				 zipFile.ExtractAll(targetDirectory);
-			}
+				    zipFile.ExtractAll(targetDirectory);
+			    }
+	  }
+
+	  static void DownloadAndDecompressFile(string remoteDownloadLink, string zipFilePath,  string targetDirectory){
+	  		   Uri uri = new Uri(remoteDownloadLink);
+    		   WebRequest http = HttpWebRequest.Create(uri.AbsoluteUri);
+    		   HttpWebResponse response = (HttpWebResponse)http.GetResponse();
+    		   Stream stream = response.GetResponseStream();
+    		   FileStream fileStream = new FileStream(zipFilePath, FileMode.Create);
+    		   stream.CopyTo(fileStream);
+    		   response.Close();
+    		   fileStream.Close();
+    		   Console.WriteLine("Downloading Gateblu-UI ....");
+    	     DecompressFile(zipFilePath, targetDirectory);
+	  }
+
+	  static void DownloadFile(string remoteDownloadLink, string targetDirectory){
+
+
 	  }
   }
   
