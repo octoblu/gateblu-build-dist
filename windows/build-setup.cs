e142d30a2b228f5e085d269ccd27d97fa548c666
kmariano <koshin.mariano@gmail.com>
2014-10-16 13:43:18 -0700

    Committing executable

--- windows/build-setup.cs
+++ windows/build-setup.cs
@@ -24,31 +24,27 @@ namespace Octoblu.Gateblu{
        #$gatebluZipFileName
        */
 
-	     DownloadAndDecompressFile("https://github.com/octoblu/gateblu-ui/archive/master.zip", tmpPath +"\\gateblu-ui.zip", tmpPath);
+	     Build.DownloadAndDecompressFile("https://github.com/octoblu/gateblu-ui/archive/master.zip", tmpPath + "\\gateblu-ui.zip", tmpPath);
 	     Directory.Move(tmpPath + "\\gateblu-ui-master", path + "\\gateblu-ui");
 	     var directoryInfo = Directory.CreateDirectory(nodePath);
-
-       DownloadAndDecompressFile("http://dl.node-webkit.org/v0.10.5/node-webkit-v0.10.5-win-ia32.zip", tmpPath + "\\node-webkit-v0.10.5-win-ia32.zip", path)
-       DownloadAndDecompressFile("http://nodejs.org/dist/npm/npm-1.4.12.zip", tmpPath + "\\npm-1.4.12.zip",  nodePath);
-       DownloadFile("http://nodejs.org/dist/v0.10.32/x64/node.exe", nodePath );
+       Build.DownloadAndDecompressFile("http://nodejs.org/dist/npm/npm-1.4.12.zip", tmpPath + "\\npm-1.4.12.zip",  nodePath);
+       Build.DownloadFile("http://nodejs.org/dist/v0.10.32/x64/node.exe", nodePath );
 
 		}
 		
-		static void DecompressFile(string filename, string targetDirectory ){
-		      Console.WriteLine("Unzipping" + filename " to " + targetDirectory);
+		public static void DecompressFile(string filename, string targetDirectory ){
 	        using (ZipFile zipFile = ZipFile.Read(filename))
 	        {
 				    zipFile.ExtractAll(targetDirectory);
 			    }
 	  }
 
-	  static void DownloadAndDecompressFile(string remoteDownloadLink, string filePath,  string targetDirectory){
-    		   DownloadFile(remoteDownloadLink, filePath)
-    	     DecompressFile(filePath, targetDirectory);
+	  public static void DownloadAndDecompressFile(string remoteDownloadLink, string filePath,  string targetDirectory){
+    		   Build.DownloadFile(remoteDownloadLink, filePath);
+    	     Build.DecompressFile(filePath, targetDirectory);
 	  }
 
-	  static void DownloadFile(string remoteDownloadLink, string filePath){
-	     Console.WriteLine("Downloading " + remoteDownloadLink + "to " + filePath);
+	  public static void DownloadFile(string remoteDownloadLink, string filePath){
        Uri uri = new Uri(remoteDownloadLink);
        WebRequest http = HttpWebRequest.Create(uri.AbsoluteUri);
        HttpWebResponse response = (HttpWebResponse)http.GetResponse();
@@ -57,8 +53,6 @@ namespace Octoblu.Gateblu{
        stream.CopyTo(fileStream);
        response.Close();
        fileStream.Close();
-       Console.WriteLine("Download complete");
-
 	  }
   }
   
