669ffb56d73d28a7b8e7b02c011e23e977130014
kmariano <koshin.mariano@gmail.com>
2014-10-16 14:09:17 -0700

    Fixing exception

--- windows/build-setup.cs
+++ windows/build-setup.cs
@@ -26,7 +26,11 @@ namespace Octoblu.Gateblu{
 
 	     Build.DownloadAndDecompressFile("https://github.com/octoblu/gateblu-ui/archive/master.zip", tmpPath + "\\gateblu-ui.zip", path);
 	     Directory.Move(path + "\\gateblu-ui-master", path + "\\gateblu-ui");
-	     var directoryInfo = Directory.CreateDirectory(nodePath);
+
+	     if(!Directory.Exists(nodePath)){
+	       Directory.CreateDirectory(nodePath);
+	     }
+
        Build.DownloadAndDecompressFile("http://nodejs.org/dist/npm/npm-1.4.12.zip", tmpPath + "\\npm-1.4.12.zip",  path);
        Build.DownloadFile("http://nodejs.org/dist/v0.10.32/x64/node.exe", nodePath );
 
